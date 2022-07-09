d40f7b9ef699ecce8ba32f1206605e04e5254049
ducksfrogs <ma_yamaki@yahoo.com>
2022-07-09 09:08:51 +0900

    ee

--- wordpress/wp-content/themes/fictionalUniv/index.php
+++ wordpress/wp-content/themes/fictionalUniv/index.php
@@ -1,1 +1,8 @@
-This is our amazing
+<?php
+  function greet($name, $color)
+  {
+    echo "<p>Hello, $name is my first function. My fv color is $color</p>";
+  }
+  greet('Jone', 'green');
+  greet("Jane", "re")
+?>
