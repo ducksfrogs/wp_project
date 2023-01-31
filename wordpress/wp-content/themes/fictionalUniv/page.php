fa337fb539f7c73542a6e41dbc496be4592161a7
ducksfrogs <ma_yamaki@yahoo.com>
2023-01-31 21:59:17 +0900

    d

--- wordpress/wp-content/themes/fictionalUniv/page.php
+++ wordpress/wp-content/themes/fictionalUniv/page.php
@@ -4,6 +4,10 @@
 
   while (have_posts()) {
   the_post(); ?>
+   <?php
+    if (1+1==2){
+      echo 'This is true.'
+    } ?>
     <h1>This is a page, not a post.</h1>
     <h2> <?php the_title(); ?></h2>
     <?php the_content(); ?>
